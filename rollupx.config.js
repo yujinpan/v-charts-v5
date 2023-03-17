f2202e0fedf1d4f9878d30635a31454d638518a1
yujinpan <1192878390@qq.com>
2023-03-17 15:47:37 +0800

    chore: add cjs and umd bundles

--- rollupx.config.js
+++ rollupx.config.js
@@ -7,9 +7,35 @@ module.exports = {
     ` * (c) 2023-${new Date().getFullYear()} yujinpan\n` +
     ' */\n',
   outputDir: 'lib',
-  inputFiles: ['**/*'],
-  stylesParseFiles: [],
-  stylesCopyFiles: ['**/*'],
   typesOutputDir: 'lib',
-  singleFile: false,
+
+  formats: Object.entries(require('./src/component-list')).flatMap(
+    ([name, { src, dist }]) => {
+      src = src.replace('src/', '');
+      dist = dist.replace('lib/', '/');
+
+      return [
+        {
+          format: 'es',
+        },
+        {
+          format: 'cjs',
+          inputFiles: [src],
+          outputFile: dist + '.common.js',
+          singleFile: true,
+        },
+        {
+          format: 'umd',
+          inputFiles: [src],
+          outputFile: dist + '.min.js',
+          singleFile: true,
+          external: /^(echarts)/,
+          outputName: name,
+          outputGlobals: {
+            echarts: 'echarts',
+          },
+        },
+      ];
+    },
+  ),
 };
