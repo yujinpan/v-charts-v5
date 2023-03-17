78cc397e9520655992dc16aaf32c1465c947994c
yujinpan <1192878390@qq.com>
2023-03-17 15:57:03 +0800

    fix: remove duplicate format

--- rollupx.config.js
+++ rollupx.config.js
@@ -9,33 +9,35 @@ module.exports = {
   outputDir: 'lib',
   typesOutputDir: 'lib',
 
-  formats: Object.entries(require('./src/component-list')).flatMap(
-    ([name, { src, dist }]) => {
-      src = src.replace('src/', '');
-      dist = dist.replace('lib/', '/');
-
-      return [
-        {
-          format: 'es',
-        },
-        {
-          format: 'cjs',
-          inputFiles: [src],
-          outputFile: dist + '.common.js',
-          singleFile: true,
-        },
-        {
-          format: 'umd',
-          inputFiles: [src],
-          outputFile: dist + '.min.js',
-          singleFile: true,
-          external: /^(echarts)/,
-          outputName: name,
-          outputGlobals: {
-            echarts: 'echarts',
-          },
-        },
-      ];
+  formats: [
+    {
+      format: 'es',
     },
-  ),
+    ...Object.entries(require('./src/component-list')).flatMap(
+      ([name, { src, dist }]) => {
+        src = src.replace('src/', '');
+        dist = dist.replace('lib/', '/');
+
+        return [
+          {
+            format: 'cjs',
+            inputFiles: [src],
+            outputFile: dist + '.common.js',
+            singleFile: true,
+          },
+          {
+            format: 'umd',
+            inputFiles: [src],
+            outputFile: dist + '.min.js',
+            singleFile: true,
+            external: /^(echarts)/,
+            outputName: name,
+            outputGlobals: {
+              echarts: 'echarts',
+            },
+          },
+        ];
+      },
+    ),
+  ],
 };
